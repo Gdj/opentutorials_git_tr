ba2be7daae97ab5318d05a76a4888c111a62ec7e
djgim <turfrain@yahoo.co.kr>
2021-03-03 15:40:36 +0900

    react-router-dom : 기본사용 활용

--- react/react-router-dom-example/react-router-dom-example.md
+++ react/react-router-dom-example/react-router-dom-example.md
@@ -11,7 +11,17 @@ npx create-react-app .
 - react-router-dom 설치
   `npm i -D react-router-dom`
 
-### react-router-dom 사요
+### react-router-dom 사용
 - BrowserRouter : 최상의 컴포넌트를 감싸주는 레퍼 컴포넌트
 - Route : 페이지 컴포넌트를 Route 로감싸줌
-  1. path 속성지정
+  1. path : 속성지정
+  2. exact : 정확한 'path'만 적용. 
+    + 없을때 '/home' 은 '/'|'/home' 두가지 실행됨.
+- Switch : 페이지를 Switch로 감싸면 감싸진 페이지가 정확한 `path`때만 적용.
+  스위치 안의 라우터는 일치하는 1개만 출력됨.
+- Link : 리로드 되지않고 페이전환
+  `a`=>`Line` 테그로, `href`=>`to`속성으로 변경
+
+- HashRouter : `BrowerRouter`대신사용하면 페이지 무시되고 #인자값전달.
+- NavLink : 'Link'와 유하하지만 페이지 값과, `class="active"` 값을 준다.
+- useParams : `path:_id`를 줌으로 파라미터를 전달할 수 있다.
--- react/react-router-dom-example/src/index.js
+++ react/react-router-dom-example/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter , Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, NavLink, useParams } from "react-router-dom";
 
 function Home(){
   return (
@@ -13,11 +13,62 @@ function Home(){
   )
 }
 
+
+var contents = [
+  {id:1, title:'HTML',  description:'HTML is...'},
+  {id:2, title:'JS',    description:'JS is...'},
+  {id:3, title:'React', description:'React is...'}
+];
+
+function Topic(){
+  var params = useParams();
+  var topic_id = params.topic_id;
+  /* 페이 없을때 */
+  var selected_topic = {
+    title:'Sorry',    description:'Not Found'
+  }
+  for(var i=0; i<contents.length; i++){
+    if(contents[i].id === Number(topic_id)){
+      selected_topic = contents[i];
+      break;
+    }
+  }
+  console.log(params);
+  return (
+    <div>
+      <h3>{selected_topic.title}</h3>
+      {selected_topic.description}
+    </div>
+  )
+}
+
 function Topics(){
+  var lis = [];
+  for(var i=0; i<contents.length; i++){
+    lis.push(<li key={contents[i].id}><NavLink  to={'/topics/'+contents[i].id}>{contents[i].title}</NavLink></li>);
+  }
   return (
     <div>
       <h2>Topics</h2>
       <p>topics...</p>
+      <ul>
+        {/* 
+        <li><NavLink to="/topics/1">html</NavLink></li>
+        <li><NavLink to="/topics/2">js</NavLink></li>
+        <li><NavLink to="/topics/3">React</NavLink></li>
+         */}
+        {lis}
+      </ul>
+      <Route path="/topics/:topic_id">
+        <Topic></Topic>
+      </Route>
+      <Switch>
+        {/* 
+        <Route path="/topics/1">html...</Route>
+        <Route path="/topics/2">js...</Route>
+        <Route path="/topics/3">React...</Route> 
+        */}
+      </Switch>
     </div>
   )
 }
@@ -36,14 +87,17 @@ function App(){
     <div>
       <h1>React Router DOM example</h1>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/topics">Topics</a></li>
-        <li><a href="/contact">Contact </a></li>
+        <li><NavLink exact to="/">Home</NavLink></li>
+        <li><NavLink to="/topics">Topics</NavLink></li>
+        <li><NavLink to="/contact">Contact </NavLink></li>
       </ul>
 
-      <Route path="/"><Home></Home></Route>
-      <Route path="/topics"><Topics></Topics></Route>
-      <Route path="/contact"><Contact></Contact></Route>
+      <Switch>
+        <Route exact path="/" ><Home></Home></Route>
+        <Route path="/topics"><Topics></Topics></Route>
+        <Route path="/contact"><Contact></Contact></Route>
+        <Route path="/">Not found Error404</Route>
+      </Switch>
     </div>
   )
 }
